fc47292a2b66f465298c605454cbb6749495914b
Kamlesh kumar <kamleshkumardbg22@gmail.com>
2023-02-27 07:12:10 +0530

    Added calcultor

---
+++ script.js
@@ -0,0 +1,8 @@
+let btn = document.querySelectorAll("button");
+let input = document.getElementById("input");
+let ans = document.getElementById("ins")
+Array.from.addEventListener("click", function (e) {
+
+})
+
+
